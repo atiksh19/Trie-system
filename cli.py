2e8cc27338fdfb7db9b1f23c3015891e7b185028
Atiksh <atiksh1909@gmail.com>
2021-07-25 20:46:05 +0530

    messing with code

--- cli.py
+++ cli.py
@@ -1,6 +1,20 @@
 import requests
 import sys
+from mysql.connector import connect, Error
 
+try:
+	connection = connect(
+		host="remotemysql.com",
+		user="kzEaB8dSjz",
+		password="5xZWr3JUQr",
+		database="kzEaB8dSjz"
+	)
+	cur = connection.cursor()
+except Error as e:
+	print(e)
+
+cur.execute("UPDATE data SET trie = %s, keywords = %s", [str({'a': {'p': {'p': {'.': '.', 'l': {'e': {'.': '.'}}}}, 'n': {'y': {'t': {'h': {'i': {'n': {'g': {'.': '.'}}}}}}}}, 'd': {'e': {'v': {'e': {'l': {'o': {'p': {'e': {'r': {'.': '.'}}}}}}}}}}), str(['app', 'apple', 'anything' 'developer'])])
+connection.commit()
 """data = requests.get("https://trie-system-atiksh.herokuapp.com/show")
 for d in data:
 	print(d)
